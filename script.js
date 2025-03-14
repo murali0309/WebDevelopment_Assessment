c4df08730edb75d81706e1dd71297343d5f2d784
murali0309 <ballmurali1319@gmail.com>
2025-03-14 08:52:45 +0000

    menu page update

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    const sidemenu = document.querySelector('.navlinkscontainer-mobile')
+    sidemenu.style.display = 'flex';
+}
+
+function closeMenu(){
+    const sidemenu = document.querySelector('.navlinkscontainer-mobile')
+    sidemenu.style.display = 'none';
+}
